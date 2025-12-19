ea71b6bd8e2527a63c335b3c5b6eb7e9a5439486
adiprathapa <aprathapa01@gmail.com>
2025-12-18 21:05:40 -0600

    chore: setup github pages and actions deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ZAM/', // Project name for GitHub Pages
   server: {
     proxy: {
       '/api': {
